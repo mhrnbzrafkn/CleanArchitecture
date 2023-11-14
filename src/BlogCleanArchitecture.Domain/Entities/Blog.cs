ef2923021c13afa023e320fdbc8afb4676225bc5
mhrnbzrafkn <mhrnbzrafkn@gmail.com>
2023-11-14 23:42:08 +0330

    initial domain layer

---
+++ src/BlogCleanArchitecture.Domain/Entities/Blog.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogCleanArchitecture.Domain.Entities
+{
+    public class Blog
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTimeOffset CreationDateTime { get; set; }
+    }
+}
